793e05d216f385827d81181c60ebe1e72405b46f
sebastian <seb@firstnoodle.io>
2020-06-30 09:40:51 +0200

    tailwind.config → added variants

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,11 @@ module.exports = {
   theme: {
     extend: {},
   },
-  variants: {},
+  variants: {
+    backgroundColor: ['responsive', 'hover', 'group-hover'],
+    borderColor: ['first', 'last', 'responsive', 'focus-within'],
+    margin: ['first', 'last', 'responsive'],
+    textColor: ['group-hover']
+  },
   plugins: [],
 }
