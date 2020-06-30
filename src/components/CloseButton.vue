db81bc73b5bef88a141a77727af66586f5b2d425
sebastian <seb@firstnoodle.io>
2020-06-30 08:44:14 +0200

    Created RenderlessTagsInput and CloseButton

---
+++ src/components/CloseButton.vue
@@ -0,0 +1,22 @@
+<template>
+    <button
+        @click="$emit('click')"
+        aria-label="Close panel"
+        class="text-gray-400 hover:text-gray-500 focus:outline-none focus:shadow-outline-blue transition ease-in-out duration-150"
+    >
+        <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M6 18L18 6M6 6l12 12"
+            />
+        </svg>
+    </button>
+</template>
+
+<script>
+export default {
+    name: "CloseButton"
+};
+</script>
